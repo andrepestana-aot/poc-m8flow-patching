636bc37a8df106e4a8baae3db157031d4dcf8b39
Andre Pestana <andre.pestana@aot-technologies.com>
2026-01-07 14:38:57 -0800

    adjusts

--- run.sh
+++ run.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
+# Create virtual environment if it doesn't exist
+[ -d .venv ] || python3 -m venv .venv
+
 source .venv/bin/activate
+
+export PYTHONPATH=/mnt/c/dev/repos/poc-m8flow-patching-2/upstream/spiffworkflow-backend:$PYTHONPATH
+export PYTHONPATH=/mnt/c/dev/repos/poc-m8flow-patching-2/upstream/spiffworkflow-backend/src:$PYTHONPATH
+
+cd /mnt/c/dev/repos/poc-m8flow-patching/upstream/spiffworkflow-backend
+uv sync --all-groups --active
+cd -
+
 
 python -m uvicorn m8flow.app:app --port 7004 --env-file "$(pwd)/.env" --app-dir "$(pwd)"
 
